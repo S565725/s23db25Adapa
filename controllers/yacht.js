9864be66337952d5126e6755a7fac8c85bc672e2
S565725 <S565725@nwmissouri.edu>
2023-11-14 23:41:29 -0600

    ss8

--- controllers/yacht.js
+++ controllers/yacht.js
@@ -109,6 +109,21 @@ exports.yacht_create_Page = function(req, res) {
    res.send(`{'error': '${err}'}`);
    }
   };
+
+
+  // Handle building the view for updating a costume.
+// query provides the id
+exports.yacht_update_Page = async function(req, res) {
+   console.log("update view for item " + req.query.id);
+   try {
+     let result = await yacht.findById(req.query.id);
+     res.render('yachtupdate', { title: 'Yacht Update', toShow: result });
+   } catch (err) {
+     res.status(500);
+     res.send(`{'error': '${err}'}`);
+   }
+ };
+ 
   
 // VIEWS
 // Handle a show all view
--- routes/yacht.js
+++ routes/yacht.js
@@ -6,10 +6,15 @@ router.get('/', yacht_controlers.yacht_view_all_Page);
 
 // GET request for one costume.
 router.get('/yacht/:id', yacht_controlers.yacht_detail);
-module.exports = router;
+
 
 /* GET detail costume page */
 router.get('/detail', yacht_controlers.yacht_view_one_Page);
 
 /* GET create costume page */
 router.get('/create', yacht_controlers.yacht_create_Page);
+
+/* GET create update page */
+router.get('/update', yacht_controlers.yacht_update_Page);
+
+module.exports = router;
